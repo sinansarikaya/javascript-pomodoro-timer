37fa7f06b5b5af50e88c4556577240c514b98957
sinansarikaya <sinansarikaya@users.noreply.github.com>
2025-10-19 13:07:36 +0000

    deploy: a33844da26a83a3848042e9cd020c61dafd4e22d

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/javascript-pomodoro-timer/sw.js', { scope: '/javascript-pomodoro-timer/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
